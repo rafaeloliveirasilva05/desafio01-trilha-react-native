e8323f1d740bacc9b128476b2d66bb6927ba4d1d
Rafael <rafael.silva@sensedata.com.br>
2021-06-15 18:05:58 -0300

    feat: Inserts functionality to add, remove, and complete a task

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,18 +11,38 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task if it's not empty
+    if(newTaskTitle === '') return
+
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    };
+
+    setTasks([...tasks, newTask])
   }
 
   function handleMarkTaskAsDone(id: number) {
-    //TODO - mark task as done if exists
+    const tasksCopy = tasks.map(task => {
+      if(task.id === id)
+        return {
+          ...task,
+          done: !task.done
+        }
+
+      return task
+    })
+
+    setTasks(tasksCopy)
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const tasksCopy = tasks.filter(task => task.id !== id)
+
+    setTasks(tasksCopy) 
   }
 
   return (
@@ -31,11 +51,11 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <MyTasksList 
-        tasks={tasks} 
-        onPress={handleMarkTaskAsDone} 
-        onLongPress={handleRemoveTask} 
+      <MyTasksList
+        tasks={tasks}
+        onPress={handleMarkTaskAsDone}
+        onLongPress={handleRemoveTask}
       />
     </>
-  )
+  );
 }
