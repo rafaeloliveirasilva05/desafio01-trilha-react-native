0e58f24425a4628647260e39a6a1e38d90e03fd3
Rafael <rafael.silva@sensedata.com.br>
2021-06-15 18:10:13 -0300

    feat: Apply style to tasks that have been completed or not

--- src/components/MyTasksList.tsx
+++ src/components/MyTasksList.tsx
@@ -28,15 +28,17 @@ export function MyTasksList({ tasks, onLongPress, onPress }: MyTasksListProps) {
         return (
           <TouchableOpacity
             testID={`button-${index}`}
+            style={item.done ? styles.taskButtonDone : styles.taskButton}
             activeOpacity={0.7}
-            //TODO - use onPress, onLongPress and style props
+            onPress={() => onPress(item.id)}
+            onLongPress={() => onLongPress(item.id)}
           >
             <View 
               testID={`marker-${index}`}
-              //TODO - use style prop 
+              style={item.done ? styles.taskMarkerDone : styles.taskMarker}
             />
             <Text 
-              //TODO - use style prop
+              style={item.done ? styles.taskTextDone : styles.taskText}
             >
               {item.title}
             </Text>
